91fd257f7d5b1f8396dbd07933028764f02d56e4
PALLAB GHOSH <gpallab405@gmail.com>
2023-08-24 19:28:06 +0530

    set the index.js file with router

---
+++ client/fitness/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter, createBrowserRouter,RouterProvider} from "react-router-dom";
+import { MemoryRouter } from 'react-router-dom';
+import "bootstrap-icons/font/bootstrap-icons.css";
+//import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
