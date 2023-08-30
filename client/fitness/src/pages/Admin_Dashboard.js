2f47b31d07f330af75bfe2295041b799fe762d19
PALLAB GHOSH <gpallab405@gmail.com>
2023-08-30 13:36:17 +0530

    Modify component
    create function for account details and fetch the data .

--- client/fitness/src/pages/Admin_Dashboard.js
+++ client/fitness/src/pages/Admin_Dashboard.js
@@ -3,7 +3,7 @@ import './Dasboard.css'
 import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
 import {Box, Button, IconButton, Typography} from '@mui/material'
 import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Sidebar from './Sidebar';
 import HomeIcon from '@mui/icons-material/Home';
@@ -21,6 +21,15 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
  const Admin_Dashboard = () => {
   const navigate=useNavigate()
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const[user_account_details,set_user_details]=useState({})
+  const{firstname,lastname,username,address,password,mobile,email,course,subscription_date,price_of_course}={...user_account_details};
+
+  useEffect(()=>{
+    axios.get(`${process.env.REACT_APP_EXPRESS_URL}/user/getuser_details`)
+    .then((res)=>{
+      set_user_details(res.data)
+    })
+  },[])
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -32,25 +41,23 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
     <Button variant='contained' size='small' color='error' onClick={toggleSidebar}>Menu</Button>
     <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
     <div className="logo">Dashboard</div>
+   <Typography variant='h5' sx={{mb:4}}> <AdminPanelSettingsIcon/>{`${firstname} ${lastname}`}</Typography>
     <ul className="menu">
-    <li className="menu-item"><HomeIcon fontSize='medium' />  Home</li>
-    <li className="menu-item"><AccountCircleIcon fontSize='medium' />Account Details</li>
-      <li className="menu-item"><AnalyticsIcon fontSize='medium' />Analytics</li>
-      <li className="menu-item"><ExitToAppIcon fontSize='medium' />Signout</li>
+    <li className="menu-item"><HomeIcon fontSize='medium' /><Link style={{color:'white'}}>Home</Link>  </li>
+    <li className="menu-item"><AccountCircleIcon fontSize='medium' /><Link style={{color:'white'}}>Account Details</Link></li>
+    <li className="menu-item"><AnalyticsIcon fontSize='medium' /><Link style={{color:'white'}}>Analytics</Link></li>
+    <li className="menu-item"><ExitToAppIcon fontSize='medium' /><Link style={{color:'white'}}>Signout</Link></li>
     </ul>
   </div>
-   <Typography variant='h3' color='white' sx={{ml:'60px'}}> 
-  
-   
-    Dashboard
-   </Typography>
+
+   <Typography variant='h3' color='white' sx={{ml:'60px'}}>  Dashboard </Typography>
     </div>
+
     <iframe 
     src="https://charts.mongodb.com/charts-fitness_tracker_db-dtmap/embed/dashboards?id=92ecd971-3954-4b7e-becf-48c60c5dbeec&theme=dark&autoRefresh=true&maxDataAge=60&showTi
     tleAndDesc=false&scalingWidth=fixed&scal
     ingHeight=fixed&&attribution=false"></iframe>
     </>
-   
   );
 };
 
