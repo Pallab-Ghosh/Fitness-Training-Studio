015fc3d011528acce5af8d30a9967bd94ae3a69f
PALLAB GHOSH <gpallab405@gmail.com>
2023-09-30 22:25:00 +0530

    Update css

--- client/fitness/src/pages/Admin_Dashboard.js
+++ client/fitness/src/pages/Admin_Dashboard.js
@@ -11,7 +11,7 @@ import AnalyticsIcon from '@mui/icons-material/Analytics';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import MailIcon from '@mui/icons-material/Mail';
-
+import DensitySmallOutlinedIcon from '@mui/icons-material/DensitySmallOutlined';
 //https://charts.mongodb.com/charts-fitness_tracker_db-dtmap
 
 //92ecd971-3954-4b7e-becf-48c60c5dbeec
@@ -67,25 +67,26 @@ return {date_details:fullDateTimeString,time_details:fullTime}
   const {date_details,time_details}=data_and_time;
   return (
    <>
-    <div style={{height:'90px',backgroundColor:'slateblue'}}>
+    <div style={{height:'90px',backgroundColor:'#013680'}}>
     <Typography variant='h2' color='white' sx={{ml:100}}>  Dashboard </Typography>
  
     <Box display='flex' flexDirection='row'>
-    <Typography><Button variant='contained' size='large' color='success' onClick={toggleSidebar} sx={{ml:0.5}}>Menu</Button></Typography>
+    <Typography><Button variant='contained' size='large' style={{color:'black',backgroundColor:' #b1cef0'}} fullWidth onClick={toggleSidebar} sx={{ml:0.5}}><DensitySmallOutlinedIcon/></Button></Typography>
     </Box>
 
     <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
-    <div className="logo">Admin Details</div>
-   <Typography variant='h5' sx={{mb:1}}> <AdminPanelSettingsIcon/>{`${firstname} ${lastname}`}</Typography>
-   <Typography variant='h6' sx={{mt:0.5,ml:3,mb:4}}><MailIcon/>{`${email}`}</Typography>
-    <ul className="menu">
-    <li className="menu-item"><HomeIcon fontSize='medium' /><a style={{color:'white'}} href='/signin/admin_login/dashboard/home'>Home</a>  </li>
-    <li className="menu-item"><AnalyticsIcon fontSize='medium' /><a style={{color:'white'}} href='/signin/admin_login/dashboard/settings'>Settings</a></li>
-    <li className="menu-item"><ExitToAppIcon fontSize='medium' /><Link style={{color:'white'}} onClick={handle_signout}>Signout</Link></li>
-    </ul>
-    <Typography variant='h5' sx={{mt:45,fontSize:'18px'}}>Today</Typography>
-    <Typography variant='h5' sx={{mt:1,fontSize:'18px'}}>{`${date_details}    ${time_details}`}</Typography>
-    <Typography variant='h5' sx={{mt:4,fontSize:'18px'}}>Fitness-Training-Studio</Typography>
+
+        <div className="logo">Admin Details</div>
+        <Typography variant='h5' sx={{mb:1}}> <AdminPanelSettingsIcon/>{`${firstname} ${lastname}`}</Typography>
+        <Typography variant='h6' sx={{mt:0.5,ml:3,mb:4}}><MailIcon/>{`${email}`}</Typography>
+        <ul className="menu">
+        <li className="menu-item"><HomeIcon fontSize='medium' /><a style={{color:'white'}} href='/signin/admin_login/dashboard/home'>Home</a>  </li>
+        <li className="menu-item"><AnalyticsIcon fontSize='medium' /><a style={{color:'white'}} href='/signin/admin_login/dashboard/settings'>Settings</a></li>
+        <li className="menu-item"><ExitToAppIcon fontSize='medium' /><Link style={{color:'white'}} onClick={handle_signout}>Signout</Link></li>
+        </ul>
+        <Typography variant='h5' sx={{mt:45,fontSize:'18px'}}>Today</Typography>
+        <Typography variant='h5' sx={{mt:1,fontSize:'18px'}}>{`${date_details}    ${time_details}`}</Typography>
+        <Typography variant='h5' sx={{mt:4,fontSize:'18px'}}>Fitness-Training-Studio</Typography>
   </div>
 
     </div>
