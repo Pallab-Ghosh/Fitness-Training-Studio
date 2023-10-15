fb4ccf02a3ba92b947054282ceb3e617d7490f5f
PALLAB GHOSH <gpallab405@gmail.com>
2023-10-15 10:42:58 +0530

    Add Footer Section

--- client/fitness/src/components/Layout.js
+++ client/fitness/src/components/Layout.js
@@ -2,13 +2,18 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
+import '../landing_page/LandingPage.css'
+
 
 export const Layout = ({children}) => {
   return (
     <div>
     <Navbar/>
   <main>{children}</main>
-    <Footer/>
+
+  <footer className='landing_page_footer'>
+  {"Powered By Fitness-Training-Studio"}
+</footer>
     </div>
   )
 }
