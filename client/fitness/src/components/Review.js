6416eb09368cac0f7b4188bc4e2e4d652bd36482
PALLAB GHOSH <gpallab405@gmail.com>
2023-10-16 20:31:01 +0530

    Update Comonent with reviews and Rating Component

--- client/fitness/src/components/Review.js
+++ client/fitness/src/components/Review.js
@@ -1,29 +1,66 @@
-import React from 'react'
-import {InputLabel, Rating,TextField,Typography} from '@mui/material';
-import { Button } from 'rsuite';
+import React, { useEffect, useState } from 'react'
+import {InputLabel, Rating,TextField,Typography,Button} from '@mui/material';
+import ReviewsIcon from '@mui/icons-material/Reviews';
+import axios from 'axios';
 
 
-const Review = ({input,setinput,value,setValue,handle_change}) => {
+const Review = () => {
+
+  const [rating, setrating] = useState(0)
+  const [review,setreview]=useState('')
+  const[user,set_user]=useState({});
+  
+  
+
+  const handle_change=(e,newvalue)=>{
+    setrating(newvalue);
+}
 
 
 
-    const handle_review=()=>{
-        console.log(input)
-    }
+useEffect(()=>{
+  var resolve_data;
+  const get_details=async()=>{
+    resolve_data =await axios.get(`${process.env.REACT_APP_EXPRESS_URL}/user/getuser_details`)
+   // console.log("resolve_data.data from get",resolve_data.data)
+    set_user(resolve_data.data);
+  }
+
+  get_details();
+
+ },[])
+
+
+
+
+    const handle_review=async()=>{
+        console.log(rating)
+        console.log(review)
+        const {_id}={...user}
+        const review_obj={rating:rating,review:review,user_id:_id};
+        console.log("review_obj",review_obj)
+        if(review_obj.rating!='' || review_obj.review!='')
+        {
+        const data=await axios.patch(`${process.env.REACT_APP_EXPRESS_URL}/user/update_review`,review_obj)
+        }
+
+        setrating(0);
+        setreview('')
+    } 
     
   return (
-    <div style={{display:'flex',flexDirection:'column' ,alignItems:'center'}}>
-    <div style={{fontSize:'25px'}}>Write a Review</div>
+    <div style={{display:'flex',flexDirection:'column' ,alignItems:'center',margin:'80px'}}>
+    <div style={{fontSize:'25px',color:'#f27a02',margin:'20px'}}>Write a Review</div>
 
     <div style={{display:'flex',flexDirection:'column',justifyContent:'center'}}>
          <div style={{display:'flex',flexDirection:'row',gap:'20px',justifyContent:'center'}}>
-             <TextField type='text' fullWidth inputProps={{style: {fontSize: 17}}} value={input} onChange={(e)=>setinput(e.target.value)} />
-             <Button  color='green' onClick={handle_review} variant='contained'  size='large' style={{fontSize:'17px',width:'200px'}} >Give Review</Button>
+             <TextField type='text'  fullWidth inputProps={{style: {fontSize: 17,maxLength :55}}} value={review} onChange={(e)=>setreview(e.target.value)} />
+              <Button  color='warning' onClick={handle_review} variant='contained'  size='large' style={{fontSize:'17px',width:'340px'}} endIcon={<ReviewsIcon/>}>Give Review</Button> 
         </div>
    
         <div style={{display:'flex',flexDirection:'column', alignItems:'center',margin:'40px'}}>
             <Typography variant='h4' sx={{width:'300px',textAlign:'center'}}>Rate this</Typography>
-            <Rating name="simple-controlled" value={value} sx={{fontSize:'50px'}} onChange={handle_change}  precision={0.5} />
+            <Rating name="simple-controlled" value={rating} sx={{fontSize:'50px'}} onChange={handle_change}  precision={0.5} />
         </div>
      
     </div>
