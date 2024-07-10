2fe5fef09d2e0fe645356d582067704cfc9ebae3
PALLAB GHOSH <gpallab405@gmail.com>
2024-07-10 18:19:00 +0530

    Change the content of dialog andin dialog add submit button for user deletion

--- client/fitness/src/components/Dialog.js
+++ client/fitness/src/components/Dialog.js
@@ -5,8 +5,10 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
 
-export default function AlertDialog({useriD , name , username}) 
+export default function AlertDialog({userId , name , username , handleDelete}) 
 {
   const [open, setOpen] = React.useState(false);
 
@@ -19,34 +21,39 @@ export default function AlertDialog({useriD , name , username})
   };
 
   return (
-    <React.Fragment>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Delete
-      </Button>
+    <> 
+             <Button 
+               onClick={handleClickOpen}fullWidth color='error'  variant='contained'  size='large' style={{fontSize:'12px',borderRadius:'12px'}} 
+               startIcon={<DeleteIcon />}
+              >
+                   Delete
+              </Button> 
 
       <Dialog
         open={open}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        sx={{borderRadius:'10px'}}
       >
-        <DialogTitle id="alert-dialog-title">
-          {` Do you want to delete ${name} with username ${username} ?`}
+        <DialogTitle id="alert-dialog-title" sx={{fontSize:'20px'}}>
+          Confirmation for user deletion 
         </DialogTitle>
 
         <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Let Google help apps determine location. This means sending anonymous
-            location data to Google, even when no apps are running.
+          <DialogContentText id="alert-dialog-description" sx={{fontSize:'20px'}}>
+           <p>Do you want to delete {name} username <span style={{fontWeight:'bolder'}}>{username}</span> from portal ?`</p>
           </DialogContentText>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Disagree</Button>
-          <Button onClick={handleClose} autoFocus>
+
+          <Button onClick={()=>handleDelete(userId)} autoFocus>
             Agree
           </Button>
+
         </DialogActions>
       </Dialog>
-    </React.Fragment>
+    </>
   );
 }
