6fd7d11a7d1b070818061a925248e4379351b2c9
PALLAB GHOSH <gpallab405@gmail.com>
2024-08-05 21:07:17 +0530

    Create a new process , make some changes for payments

--- server/controller/payment.js
+++ server/controller/payment.js
@@ -3,12 +3,62 @@ const stripe = require('stripe')(process.env.Secret_key_stripe)
 const { send_mail_subscription } = require('../utils/Send_Mail_For_Subscriptions');
 
 
-exports.payment=(async (req, res) => {
+/* exports.payment=(async (req, res) => {
     const {newdata}=req.body;
     const{id_of_package,title_of_package,price_of_package,email,firstname,lastname}=newdata;
     console.log(id_of_package,title_of_package,price_of_package,email,firstname,lastname);
 
   const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: 'USD',
+          product_data: {
+            name: title_of_package,
+          },
+          unit_amount: price_of_package*100,
+        },
+        quantity: 1,
+      },
+    ],
+    billing_address_collection: 'auto',
+    shipping_address_collection: {
+      allowed_countries: ['US'],
+    }, 
+   
+    mode: 'payment',
+    success_url: `${process.env.REACT_URL}/home/get_admission/checkout_success/?success=1`,
+    cancel_url: `${process.env.REACT_URL}/home/get_admission/?cancel=1`,
+  });
+
+  
+  res.send({url: session.url});
+});
+
+
+
+
+
+
+ */
+ 
+
+ 
+
+exports.payment = async (req, res) => {
+
+  const {newdata}=req.body;
+  const{id_of_package,title_of_package,price_of_package,email,firstname,lastname}=newdata;
+  console.log(id_of_package,title_of_package,price_of_package,email,firstname,lastname);
+  const session = await stripe.checkout.sessions.create({
+ 
+    
+    billing_address_collection: 'auto',
+
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA'],
+    },
+
     line_items: [
       {
         price_data: {
@@ -21,15 +71,12 @@ exports.payment=(async (req, res) => {
         quantity: 1,
       },
     ],
-    phone_number_collection:{
-      enabled:true
-    },
+
     mode: 'payment',
-    success_url: `${process.env.REACT_URL}/home/get_admission/checkout_success/?success=1`,
-    cancel_url: `${process.env.REACT_URL}/home/get_admission/?cancel=1`,
+    success_url: `${process.env.REACT_APP_BASE_URL}/home/get_admission/checkout_success/?success=1`,
+    cancel_url: `${process.env.REACT_APP_BASE_URL}/home/get_admission/?cancel=1`,
   });
 
-  
-  res.send({url: session.url});
-});
-
+   res.send({url:session.url})
+}
+ 
